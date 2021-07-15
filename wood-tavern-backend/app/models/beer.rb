7e982d05d8963dafba8a1386c14f70de6da66334
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-14 22:29:07 -0500

    added presence validations in Beer model for name, style, abv, brewery and location attributes, and uniqueness validation for name attribute

--- wood-tavern-backend/app/models/beer.rb
+++ wood-tavern-backend/app/models/beer.rb
@@ -1,2 +1,7 @@
 class Beer < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :style, presence: true
+    validates :abv, presence: true
+    validates :brewery, presence: true
+    validates :location, presence: true
 end
