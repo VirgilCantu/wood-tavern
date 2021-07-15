86a141d3f8d8833e2f29199527076d6682665111
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 14:01:11 -0500

    imported Provider function from react-redux package, createStore and applyMiddleware functions from redux package and thunk from redux-thunk package

--- wood-tavern-frontend/src/index.js
+++ wood-tavern-frontend/src/index.js
@@ -4,6 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
