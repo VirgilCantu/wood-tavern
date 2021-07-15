a9559c5cbe86472eae9ad8b4673a975aff8550fe
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-14 22:17:53 -0500

    used rails scaffold generator to create routes, controller and migration for Beer model

---
+++ wood-tavern-backend/db/migrate/20210715031344_create_beers.rb
@@ -0,0 +1,12 @@
+class CreateBeers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :beers do |t|
+      t.string :style
+      t.string :abv
+      t.string :brewery
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
