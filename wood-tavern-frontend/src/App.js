f1e8d9443065b0a7efc18565e62eb713955f9a83
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 18:07:43 -0500

    added Router component within App functional component return statement, Router component now wraps Nav component, also wraps Route component which renders Home component

--- wood-tavern-frontend/src/App.js
+++ wood-tavern-frontend/src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Nav />
+        <Route exact path="/" component={Home} />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
