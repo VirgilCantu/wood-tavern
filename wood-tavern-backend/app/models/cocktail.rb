b2ffc8381e5b3dc993882f6508fd19a5dc2292d3
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 12:32:23 -0500

    removed image atrtibute validation from Cocktail model

--- wood-tavern-backend/app/models/cocktail.rb
+++ wood-tavern-backend/app/models/cocktail.rb
@@ -1,6 +1,5 @@
 class Cocktail < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :price, presence: true
-    validates :image, presence: true
     validates :description, presence: true
 end
