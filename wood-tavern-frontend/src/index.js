07ce0eb2757761a831113391e5fcfd37ef67a6b0
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 14:16:44 -0500

    added another argument to createStore function in index.js to enable thunk middleware

--- wood-tavern-frontend/src/index.js
+++ wood-tavern-frontend/src/index.js
@@ -20,6 +20,7 @@ const rootReducer = combineReducers({
 
 const store = createStore(
   rootReducer,
+  applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
