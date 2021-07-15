8800ea00114e2b469cd85d39bc4a893c542d4209
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-14 21:54:34 -0500

    added validations for presence in Cocktail model for name, glassware, ice, image and preparation attributes, and a uniqueness validation for name attribute

--- wood-tavern-backend/app/models/cocktail.rb
+++ wood-tavern-backend/app/models/cocktail.rb
@@ -1,4 +1,10 @@
 class Cocktail < ApplicationRecord
     has_many :cocktail_ingredients
     has_many :ingredients, through: :cocktail_ingredients
+
+    validates :name, presence: true, uniqueness: true
+    validates :glassware, presence: true
+    validates :ice, presence: true
+    validates :preparation, presence: true
+    validates :image, presence: true
 end
