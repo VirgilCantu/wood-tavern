c6b0919278eb4822addb77aa6a7d44d36ac1b67f
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-14 21:02:06 -0500

    used rails scaffold generator to create routes, controller and migration for Ingredient model

---
+++ wood-tavern-backend/db/migrate/20210715020107_create_ingredients.rb
@@ -0,0 +1,11 @@
+class CreateIngredients < ActiveRecord::Migration[6.1]
+  def change
+    create_table :ingredients do |t|
+      t.string :name
+      t.string :category
+      t.string :quantity
+
+      t.timestamps
+    end
+  end
+end
