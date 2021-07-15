94df16bd64a07acc2a521e949fe0be5a24282541
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 01:51:26 -0500

    created 38 Beer model objects in seed data

--- wood-tavern-backend/db/seeds.rb
+++ wood-tavern-backend/db/seeds.rb
@@ -1,7 +1,345 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#----------------------Beer - Draft--------------------#
+
+agave_wheat = Beer.create(
+    name: "Agave Wheat",
+    format: "Draft",
+    style: "Wheat Beer - American Pale",
+    abv: "4.2%",
+    brewery: "Breckenridge Brewery",
+    location: "Colorado, United States"
+)
+
+toasted_lager = Beer.create(
+    name: "Toasted Lager",
+    format: "Draft",
+    style: "Lager - American Amber / Red",
+    abv: "5.5%",
+    brewery: "Blue Point Brewing Company",
+    location: "New York, United States"
+)
+
+jai_alai = Beer.create(
+    name: "Jai Alai IPA",
+    format: "Draft",
+    style: "IPA - American",
+    abv: "7.5%",
+    brewery: "Cigar City Brewing",
+    location: "Florida, United States"
+)
+
+dirty_bastard = Beer.create(
+    name: "Dirty Bastard",
+    format: "Draft",
+    style: "Scotch Ale / Wee Heavy",
+    abv: "8.5%",
+    brewery: "Founders Brewing Company",
+    location: "Michigan, United States"
+)
+
+fat_tire = Beer.create(
+    name: "Fat Tire",
+    format: "Draft",
+    style: "Red Ale - American Amber / Red",
+    abv: "5.2%",
+    brewery: "New Belgium Brewing Company",
+    location: "Colorado, United States"
+)
+
+centennial_ipa = Beer.create(
+    name: "Centennial IPA",
+    format: "Draft",
+    style: "IPA - American",
+    abv: "7.2%",
+    brewery: "Founders Brewing Company",
+    location: "Michigan, United States"
+)
+
+hoptical_illusion = Beer.create(
+    name: "Hoptical Illusion",
+    format: "Draft",
+    style: "IPA - American",
+    abv: "7%",
+    brewery: "Blue Point Brewing Company",
+    location: "New York, United States"
+)
+
+number_9 = Beer.create(
+    name: "#9",
+    format: "Draft",
+    style: "Fruit and Field Beer",
+    abv: "5.1%",
+    brewery: "Magic Hat Brewing Company",
+    location: "Vermont, United States"
+)
+
+monk_in_the_trunk = Beer.create(
+    name: "Monk in the Trunk",
+    format: "Draft",
+    style: "Red Ale - American Amber / Red",
+    abv: "5.5%",
+    brewery: "Inlet Brewing Co.",
+    location: "Florida, United States"
+)
+
+rastafarye_ale = Beer.create(
+    name: "RastafaRye Ale",
+    format: "Draft",
+    style: "Rye Beer",
+    abv: "7.5%",
+    brewery: "Blue Point Brewing Company",
+    location: "New York, United States"
+)
+
+pumpkinhead_ale = Beer.create(
+    name: "Pumpkinhead Ale",
+    format: "Draft",
+    style: "Pumpkin Beer",
+    abv: "4.5%",
+    brewery: "Shipyard Brewing Company",
+    location: "Maine, United States"
+)
+
+2xipa = Beer.create(
+    name: "2XIPA",
+    format: "Draft",
+    style: "IPA - Imperial",
+    abv: "8.2%",
+    brewery: "Southern Tier Brewing Company",
+    location: "New York, United States"
+)
+
+wynwood_ipa = Beer.create(
+    name: "Wynwood IPA",
+    format: "Draft",
+    style: "IPA - American",
+    abv: "7.2%",
+    brewery: "Wynwood Brewing Company",
+    location: "Florida, United States"
+)
+
+pops_porter = Beer.create(
+    name: "Pop's Porter",
+    format: "Draft",
+    style: "Porter - American",
+    abv: "6.2%",
+    brewery: "Wynwood Brewing Company",
+    location: "Florida, United States"
+)
+
+lupulado = Beer.create(
+    name: "Lupulado",
+    format: "Draft",
+    style: "Pale Ale - American",
+    abv: "4.5%",
+    brewery: "Wynwood Brewing Company",
+    location: "Florida, United States"
+)
+
+la_rubia = Beer.create(
+    name: "La Rubia",
+    format: "Draft",
+    style: "Blonde Ale - American",
+    abv: "5%",
+    brewery: "Wynwood Brewing Company",
+    location: "Florida, United States"
+)
+
+#----------------------Beer - Bottled--------------------#
+
+amstel_light = Beer.create(
+    name: "Amstel Light",
+    format: "Bottle",
+    style: "Lager - Light",
+    abv: "3.5%",
+    brewery: "Amstel Brouwerij B.V.",
+    location: "Netherlands"
+)
+
+anchor_steam = Beer.create(
+    name: "Anchor Steam",
+    format: "Bottle",
+    style: "California Common / Steam Beer",
+    abv: "4.9%",
+    brewery: "Anchor Brewing Company",
+    location:  "California, United States"
+)
+
+sculpin = Beer.create(
+    name: "Sculpin",
+    format: "Bottle",
+    style: "IPA - American",
+    abv: "7%",
+    brewery: "Ballast Point Brewing Company",
+    location:  "California, United States"
+)
+
+brown_ale = Beer.create(
+    name: "Brown Ale",
+    format: "Bottle",
+    style: "Brown Ale - American",
+    abv: "5.6%",
+    brewery: "Brooklyn Brewery",
+    location:  "New York, United States"
+)
+
+lager = Beer.create(
+    name: "Lager",
+    format: "Bottle",
+    style: "Lager - American Amber / Red",
+    abv: "5.2%",
+    brewery: "Brooklyn Brewery",
+    location:  "New York, United States"
+)
+
+60_minute_ipa = Beer.create(
+    name: "60 Minute IPA",
+    format: "Bottle",
+    style: "IPA - American",
+    abv: "6%",
+    brewery: "Dogfish Head Craft Brewery",
+    location:  "Delaware, United States"
+)
+
+90_minute_ipa = Beer.create(
+    name: "90 Minute IPA",
+    format: "Bottle",
+    style: "IPA - Imperial",
+    abv: "9%",
+    brewery: "Dogfish Head Craft Brewery",
+    location:  "Delaware, United States"
+)
+
+heineken_lager_beer = Beer.create(
+    name: "Heineken Lager",
+    format: "Bottle",
+    style: "Lager - European Pale",
+    abv: "5%",
+    brewery: "Heineken Nederland B.V.",
+    location:  "Netherlands"
+)
+
+milk_stout_nitro = Beer.create(
+    name: "Milk Stout Nitro",
+    format: "Bottle",
+    style: "Stout - Sweet / Milk",
+    abv: "6%",
+    brewery: "Left Hand Brewing Company",
+    location:  "Colorado, United State"
+)
+
+stranger = Beer.create(
+    name: "Stranger",
+    format: "Bottle",
+    style: "Pale Ale - American",
+    abv: "5%",
+    brewery: "Left Hand Brewing Company",
+    location:  "Colorado, United State"
+)
+
+cinco_estrellas = Beer.create(
+    name: "Cinco Estrellas",
+    format: "Bottle",
+    style: "Lager - European Pale",
+    abv: "5.5%",
+    brewery: "Mahou S.A.",
+    location:  "Spain"
+)
+
+seance = Beer.create(
+    name: "Séance",
+    format: "Bottle",
+    style: "Farmhouse Ale - Saison",
+    abv: "4.4%",
+    brewery: "Magic Hat Brewing Company",
+    location: "Vermont, United States"
+)
+
+medalla_light = Beer.create(
+    name: "Medalla Light",
+    format: "Bottle",
+    style: "Lager - Light",
+    abv: "4.2%",
+    brewery: "Compañía Cervecera de Puerto Rico",
+    location: "Puerto Rico"
+)
+
+modelo_especial = Beer.create(
+    name: "Modelo Especial",
+    format: "Bottle",
+    style: "Lager - Adjunct",
+    abv: "4.4%",
+    brewery: "Grupo Modelo S.A. de C.V.",
+    location: "Mexico"
+)
+
+humboldt_brown = Beer.create(
+    name: "Humboldt Brown",
+    format: "Bottle",
+    style: "Brown Ale - American",
+    abv: "5.7%",
+    brewery: "Humboldt Brewing Co.",
+    location: "South Carolina, United States"
+)
+
+humboldt_brown = Beer.create(
+    name: "Humboldt Brown",
+    format: "Bottle",
+    style: "Brown Ale - American",
+    abv: "5.7%",
+    brewery: "Humboldt Brewing Co.",
+    location: "South Carolina, United States"
+)
+
+palm = Beer.create(
+    name: "Pale",
+    format: "Bottle",
+    style: "Pale Ale - Belgian",
+    abv: "5.4%",
+    brewery: "Brouwerij Palm NV",
+    location: "Belgium"
+)
+
+dead_guy_ale = Beer.create(
+    name: "Dead Guy Ale",
+    format: "Bottle",
+    style: "Bock - Malbock",
+    abv: "6.8%",
+    brewery: "Rogue Ales",
+    location: "Oregon, United States"
+)
+
+sea_dog_blueberry = Beer.create(
+    name: "Sea Dog Blueberry",
+    format: "Bottle",
+    style: "Fruit and Field Beer",
+    abv: "4.7%",
+    brewery: "Shipyard Brewing Company",
+    location: "Maine, United States"
+)
+
+stella_artois = Beer.create(
+    name: "Stella Artois",
+    format: "Bottle",
+    style: "Lager - European Pale",
+    abv: "5%",
+    brewery: "Stella Artois",
+    location: "Belgium"
+)
+
+prima_pils = Beer.create(
+    name: "Prima Pils",
+    format: "Bottle",
+    style: "Pilsner - German",
+    abv: "5.3%",
+    brewery: "Victory Brewing Company",
+    location: "Pennsylvania, United States"
+)
+
+amber = Beer.create(
+    name: "Amber Hard Cider",
+    format: "Bottle",
+    style: "Cider - Traditional",
+    abv: "5%",
+    brewery: "Woodchuck Cidery",
+    location: "Vermont, United States"
+)
