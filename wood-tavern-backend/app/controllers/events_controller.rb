de14d91741ee445c89885fa5a836085c30e47e3f
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 13:38:26 -0500

    removed before_action and set_event method in EventsController, edited show action to reflect this change, and to display a message when an Event is not found

--- wood-tavern-backend/app/controllers/events_controller.rb
+++ wood-tavern-backend/app/controllers/events_controller.rb
@@ -1,5 +1,4 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :update, :destroy]
 
   # GET /events
   def index
@@ -10,7 +9,12 @@ class EventsController < ApplicationController
 
   # GET /events/1
   def show
-    render json: event
+    event = Event.find_by(id: params[:id])
+    if event
+      render json: EventSerializer.new(event).to_serialized_json
+    else
+      render json: { message: 'Event not found' }
+    end
   end
 
   # POST /events
@@ -39,11 +43,6 @@ class EventsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      event = Event.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def event_params
       params.require(:event).permit(:name, :time, :date, :description)
