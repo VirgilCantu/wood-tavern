a61a794d5cbd6d0dfc096672c1f31723bde4a71d
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 15:26:05 -0500

    changed App.js component from functional component to class component

--- wood-tavern-frontend/src/App.js
+++ wood-tavern-frontend/src/App.js
@@ -1,5 +1,9 @@
-function App() {
-  return <div></div>;
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div></div>;
+  }
 }
 
 export default App;
