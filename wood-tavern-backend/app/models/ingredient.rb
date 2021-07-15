a9f7a4aedd91cbad519ebcc8a582d4fbe6252402
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 02:01:00 -0500

    used rails delete scaffold to remove Ingredient model and all associated resources, no longer necessary

--- wood-tavern-backend/app/models/ingredient.rb
+++
@@ -1,8 +0,0 @@
-class Ingredient < ApplicationRecord
-    has_many :cocktail_ingredients
-    has_many :cocktails, through: :cocktail_ingredients
-
-    validates :name, presence: true
-    validates :category, presence: true
-    validates :quantity, presence: true
-end
