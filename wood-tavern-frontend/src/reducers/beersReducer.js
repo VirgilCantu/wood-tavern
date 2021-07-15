06958d6543ccf11fb12244a1b461b8f4ff91930d
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 15:17:30 -0500

    added ADD_BEERS case to beersReducer function

--- wood-tavern-frontend/src/reducers/beersReducer.js
+++ wood-tavern-frontend/src/reducers/beersReducer.js
@@ -6,6 +6,12 @@ const beersReducer = (state = { beers: [], loading: false }, action) => {
         beers: [...state.beers],
         loading: true
       };
+    case "ADD_BEERS":
+      return {
+        ...state,
+        beers: action.beers,
+        loading: false
+      };
     default:
       return state;
   }
