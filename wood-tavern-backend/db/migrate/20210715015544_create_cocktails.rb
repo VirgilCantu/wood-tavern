ccfcfb69b3f72f1e2ee004d923dec7ec54241642
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 01:56:19 -0500

    changed attributes in create_cocktails migration, new attributes will be name, price, image, and description

--- wood-tavern-backend/db/migrate/20210715015544_create_cocktails.rb
+++ wood-tavern-backend/db/migrate/20210715015544_create_cocktails.rb
@@ -2,11 +2,9 @@ class CreateCocktails < ActiveRecord::Migration[6.1]
   def change
     create_table :cocktails do |t|
       t.string :name
-      t.string :glassware
-      t.string :ice
+      t.string :price
       t.string :image
-      t.string :origin
-      t.string :preparation
+      t.string :description
 
       t.timestamps
     end
