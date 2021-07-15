90fecc3f8b73d57f36284800f063776702ec9d96
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 17:47:02 -0500

    turned App component back into a functional component, state not required, imported BrowserRouter as Router and Route from react-router-dom package

--- wood-tavern-frontend/src/App.js
+++ wood-tavern-frontend/src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
