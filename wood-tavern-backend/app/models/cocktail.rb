c847233b0bef1bf28f978174d48096365f926518
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 02:05:21 -0500

    removed has_many relationships in Cocktail model to reflect deletion of Cocktail and CocktailIngredient models

--- wood-tavern-backend/app/models/cocktail.rb
+++ wood-tavern-backend/app/models/cocktail.rb
@@ -1,7 +1,4 @@
 class Cocktail < ApplicationRecord
-    has_many :cocktail_ingredients
-    has_many :ingredients, through: :cocktail_ingredients
-
     validates :name, presence: true, uniqueness: true
     validates :price, presence: true
     validates :image, presence: true
