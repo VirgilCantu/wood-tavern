695fb4015d32a464c6b3dc753c0bf425c388fcd7
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-15 18:10:54 -0500

    imported and added Link component from react-router-dom into Nav component, linking to home page/Home component

--- wood-tavern-frontend/src/components/Nav.js
+++ wood-tavern-frontend/src/components/Nav.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+    </div>
+  );
 };
 
 export default Nav;
