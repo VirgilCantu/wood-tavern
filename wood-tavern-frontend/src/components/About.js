72052062cb43d539298ef0cbb9169e38b1ca0195
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 00:21:10 -0500

    added h1 to Home.js component, also added h1 and p tags to About.js component

--- wood-tavern-frontend/src/components/About.js
+++ wood-tavern-frontend/src/components/About.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const About = () => {
-  return <div>This is About</div>;
+  return (
+    <div>
+      <h1>Who We Are</h1>
+      <p>
+        The idea behind Wood Tavern has always been simple. Open a place where
+        everyone wants to be, because anyone is welcome. Where you can meet up
+        with friends, grab a craft brew or a cocktail, and wait to see which
+        character you bump into this time. And there’s always good music, so
+        catch a picnic table with your friends on the patio. People don’t just
+        come here to drink. They come to Wood to get comfortable.
+      </p>
+    </div>
+  );
 };
 
 export default About;
--- wood-tavern-frontend/src/components/Home.js
+++ wood-tavern-frontend/src/components/Home.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Wood Tavern</h1>
+    </div>
+  );
 };
 
 export default Home;
