84f014c0e5096c6df6c58241c794fdf7da42f620
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 15:01:27 -0500

    built out renderCocktails method in Cocktails component to map through cocktails prop and render a Cocktail component for each item

--- wood-tavern-frontend/src/components/Cocktails.js
+++ wood-tavern-frontend/src/components/Cocktails.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 import Cocktail from "./Cocktail";
 
 class Cocktails extends Component {
+  renderCocktails = () =>
+    this.props.cocktails.map(cocktail => (
+      <Cocktail key={cocktail.id} cocktail={cocktail} />
+    ));
+
   render() {
-    return <div></div>;
+    return <div>{this.renderCocktails()}</div>;
   }
 }
 
