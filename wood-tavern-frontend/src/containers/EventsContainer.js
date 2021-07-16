1e41a35148e4aa33219e1a67fc922ce40e333579
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 16:49:31 -0500

    imported Events component, fetchEvents function, and connect function into EventsContainer.js

--- wood-tavern-frontend/src/containers/EventsContainer.js
+++ wood-tavern-frontend/src/containers/EventsContainer.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Events from "../components/events/Events";
+import { fetchEvents } from "../actions/eventActions";
+import { connect } from "react-redux";
 
 class EventsContainer extends Component {
   render() {
