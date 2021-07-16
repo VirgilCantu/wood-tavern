e52ece817526582692454d50341aca785de7cfb9
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 17:18:15 -0500

    built out Event component to display information passed in from props

--- wood-tavern-frontend/src/components/events/Event.js
+++ wood-tavern-frontend/src/components/events/Event.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Event = () => {
-  return <div></div>;
+const Event = props => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h3>
+        {props.date} - {props.time}
+      </h3>
+      <p>{props.description}</p>
+    </div>
+  );
 };
 
 export default Event;
