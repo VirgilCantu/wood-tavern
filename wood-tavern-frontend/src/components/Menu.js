0bd806b56e9b6d562b03dc5c52928995bd304dc3
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 13:47:36 -0500

    added import statements for fetchBeers and fetchCocktails functions in Menu.js, also built out mapDispatchToProps function in same file

--- wood-tavern-frontend/src/components/Menu.js
+++ wood-tavern-frontend/src/components/Menu.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { fetchBeers } from "../actions/beerActions";
+import { fetchCocktails } from "../actions/cocktailActions";
 
 class Menu extends Component {
   render() {
@@ -11,6 +13,13 @@ const mapStateToProps = state => {
   return {
     beers: state.beers,
     cocktails: state.cocktails
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchBeers: () => dispatch(fetchBeers()),
+    fetchCocktails: () => dispatch(fetchCocktails())
   };
 };
 
