ad5bf908258411ab6427e4503aa5eb71a8d0c8d1
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-17 13:24:34 -0500

    installed react-router-bootstrap package, imported LinkContainerinto Navigation.js

--- wood-tavern-frontend/src/components/Navigation.js
+++ wood-tavern-frontend/src/components/Navigation.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const link = {
   width: "100px",
