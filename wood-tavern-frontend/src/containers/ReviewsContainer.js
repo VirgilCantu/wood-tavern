6b8655aeebbe74ece8c957acf702672048167e8e
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 22:44:54 -0500

    fixed typo in ReviewsContainer component where prop was being passed down as review instead of reviews

--- wood-tavern-frontend/src/containers/ReviewsContainer.js
+++ wood-tavern-frontend/src/containers/ReviewsContainer.js
@@ -11,7 +11,7 @@ class ReviewsContainer extends Component {
   render() {
     return (
       <div>
-        <Reviews review={this.props.reviews} />
+        <Reviews reviews={this.props.reviews} />
       </div>
     );
   }
