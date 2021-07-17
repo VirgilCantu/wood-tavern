5cbf08908ed6587b11fc0b2731ce182d53f822fa
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 21:18:41 -0500

    turned all instance variables in reviews_controller into local variables

--- wood-tavern-backend/app/controllers/reviews_controller.rb
+++ wood-tavern-backend/app/controllers/reviews_controller.rb
@@ -3,45 +3,45 @@ class ReviewsController < ApplicationController
 
   # GET /reviews
   def index
-    @reviews = Review.all
+    reviews = Review.all
 
-    render json: @reviews
+    render json: reviews
   end
 
   # GET /reviews/1
   def show
-    render json: @review
+    render json: review
   end
 
   # POST /reviews
   def create
-    @review = Review.new(review_params)
+    review = Review.new(review_params)
 
-    if @review.save
-      render json: @review, status: :created, location: @review
+    if review.save
+      render json: review, status: :created, location: review
     else
-      render json: @review.errors, status: :unprocessable_entity
+      render json: review.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /reviews/1
   def update
-    if @review.update(review_params)
-      render json: @review
+    if review.update(review_params)
+      render json: review
     else
-      render json: @review.errors, status: :unprocessable_entity
+      render json: review.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /reviews/1
   def destroy
-    @review.destroy
+    review.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_review
-      @review = Review.find(params[:id])
+      review = Review.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
