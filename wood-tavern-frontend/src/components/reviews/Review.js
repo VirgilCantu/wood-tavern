d92f4a77c95ef0a3f46773bed35be2e10a790a70
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 22:21:27 -0500

    built out Review component to properly display information based on passed down review prop

--- wood-tavern-frontend/src/components/reviews/Review.js
+++ wood-tavern-frontend/src/components/reviews/Review.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const Review = () => {
-  return <div></div>;
+const Review = props => {
+  return (
+    <div>
+      <h2>
+        {props.review.title} - {props.review.rating}
+      </h2>
+      <h4>
+        Posted By: {props.review.name} on {props.review.date}
+      </h4>
+      <p>{props.review.content}</p>
+    </div>
+  );
 };
 
 export default Review;
