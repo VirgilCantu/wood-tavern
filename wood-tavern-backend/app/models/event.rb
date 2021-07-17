0592e029ae593490c627e61c5b9f142f728b8afc
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 21:14:22 -0500

    added name, date, time and description presence validations for Event model

--- wood-tavern-backend/app/models/event.rb
+++ wood-tavern-backend/app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+    validates :name, presence: true
+    validates :date, presence: true
+    validates :time, presence: true
+    validates :description, presence: true
 end
