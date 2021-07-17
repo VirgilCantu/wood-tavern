43679847de3746497ce4494868ec84624fa27038
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 21:48:09 -0500

    imported Reviews component, fetchReviews function and connect function into ReviewsContainer.js

--- wood-tavern-frontend/src/containers/ReviewsContainer.js
+++ wood-tavern-frontend/src/containers/ReviewsContainer.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Reviews from "../components/reviews/Reviews";
+import { fetchReviews } from "../actions/reviewActions";
+import { connect } from "react-redux";
 
 class ReviewsContainer extends Component {
   render() {
