691e541ee8e3b60a395180eb6340a8108ea29f28
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-17 03:04:06 -0500

    rearranged display of data in Review component

--- wood-tavern-frontend/src/components/reviews/Review.js
+++ wood-tavern-frontend/src/components/reviews/Review.js
@@ -3,12 +3,10 @@ import React from "react";
 const Review = props => {
   return (
     <div>
-      <h2>
-        {props.review.title} - {props.review.rating}
-      </h2>
-      <h4>
-        Posted By: {props.review.name} on {props.review.date}
-      </h4>
+      <h3>
+        {props.review.name} on {props.review.date}
+      </h3>
+      <h4>{props.review.rating}</h4>
       <p>{props.review.content}</p>
     </div>
   );
