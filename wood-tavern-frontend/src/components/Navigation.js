7a16d1eb020d36a35e418c6b22a71e25ee74470f
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-17 13:27:37 -0500

    deleted link const styling object in Navigation.js, deleted contents of Navigation.js return statement, will use imported react-bootstrap and react-router-bootstrap to re-write component with proper styling

--- wood-tavern-frontend/src/components/Navigation.js
+++ wood-tavern-frontend/src/components/Navigation.js
@@ -3,37 +3,8 @@ import { NavLink } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
-const link = {
-  width: "100px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  textDecoration: "none",
-  color: "white"
-};
-
 const Navigation = () => {
-  return (
-    <div>
-      <NavLink to="/" exact style={link}>
-        Home
-      </NavLink>
-      <NavLink to="/about" exact style={link}>
-        About
-      </NavLink>
-      <NavLink to="/beers" exact style={link}>
-        Beer
-      </NavLink>
-      <NavLink to="/cocktails" exact style={link}>
-        Cocktails
-      </NavLink>
-      <NavLink to="/events" exact style={link}>
-        Events
-      </NavLink>
-      <NavLink to="/reviews" exact style={link}>
-        Reviews
-      </NavLink>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Navigation;
