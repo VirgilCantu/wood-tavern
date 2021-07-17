1c5bc1d623e2137f60e3a5b963064adc33629454
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-16 21:15:22 -0500

    added validations for name, content, date and rating in Review model

--- wood-tavern-backend/app/models/review.rb
+++ wood-tavern-backend/app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+    validates :name, presence: true
+    validates :content, presence: true
+    validates :date, presence: true
+    validates :rating, presence: true
 end
