a1458fbd47e9e8b697c1656b50929eae82bc02b8
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-18 22:18:02 -0500

    added break tag for styling and inline style props to About component

--- wood-tavern-frontend/src/components/About.js
+++ wood-tavern-frontend/src/components/About.js
@@ -3,8 +3,9 @@ import React from "react";
 const About = () => {
   return (
     <div className="text-center">
+      <br />
       <h1>Who We Are</h1>
-      <p>
+      <p style={{ width: "60rem", margin: "auto" }}>
         The idea behind Wood Tavern has always been simple. Open a place where
         everyone wants to be, because anyone is welcome. Where you can meet up
         with friends, grab a craft brew or a cocktail, and wait to see which
