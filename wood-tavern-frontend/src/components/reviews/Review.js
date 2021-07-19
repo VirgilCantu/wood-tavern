ebe1ace0d70ad2cb26b84e51ac6eb75efab80eee
Virgil Cantu <cantu.sampsom@gmail.com>
2021-07-19 01:31:17 -0500

    changed h3 tag in Review component to h5, removed h4 tag, added break tags and hr tag for styling

--- wood-tavern-frontend/src/components/reviews/Review.js
+++ wood-tavern-frontend/src/components/reviews/Review.js
@@ -4,18 +4,19 @@ import StarRatings from "react-star-ratings";
 const Review = props => {
   return (
     <div>
-      <h3>
+      <h5>
         {props.review.name} on {props.review.date}
-      </h3>
-      <h4>
-        <StarRatings
-          rating={props.review.rating}
-          starDimension="20px"
-          starSpacing="5px"
-          starRatedColor="rgb(255,202,40)"
-        />
-      </h4>
+      </h5>
+      <StarRatings
+        rating={props.review.rating}
+        starDimension="20px"
+        starSpacing="5px"
+        starRatedColor="rgb(255,202,40)"
+      />
+      <br />
+      <br />
       <p>{props.review.content}</p>
+      <hr />
     </div>
   );
 };
